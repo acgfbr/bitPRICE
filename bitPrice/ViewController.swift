a8fa7b71bfb48485597c39494e05450992ed8661
Antônio Guimarães <antonio@vendala.com.br>
2019-10-01 10:20:14 -0300

    fix on divisor

--- bitPrice/ViewController.swift
+++ bitPrice/ViewController.swift
@@ -64,7 +64,7 @@ class ViewController: UIViewController {
 extension Double {
 	/// Rounds the double to decimal places value
 	func rounded(toPlaces places:Int) -> Double {
-		let divisor = pow(10.0, Double(places))
+		let divisor = pow(1.0, Double(places))
 		return (self * divisor).rounded() / divisor
 	}
 }
